2d507e864e7e7c8f387f61c9ba7da9452ad1e08e
Jussara <jussara.jandrade1993@outlook.com>
2020-11-07 20:19:43 -0300

    ajustes no método put do pacote controller, incluindo permissão para alterar

--- src/main/java/br/com/coop/coop_api/controllers/DadosOngController.java
+++ src/main/java/br/com/coop/coop_api/controllers/DadosOngController.java
@@ -2,6 +2,7 @@ package br.com.coop.coop_api.controllers;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,47 +36,29 @@ public class DadosOngController {
 	}
 	
 	@PutMapping("/editar/{id}")
-	public UsuarioOng alterar(@PathVariable int id, @RequestBody UsuarioOng ong) throws Exception {
-		UsuarioOng ongDB = repository.findById(id).orElseThrow(() -> new IllegalAccessException());
+	public UsuarioOng alterar(@PathVariable("id") int id, @RequestBody UsuarioOng ong) throws Exception {
+		UsuarioOng ongBD = repository.findById(id)
+				.orElseThrow(() -> new IllegalAccessException());
 		
-		if(ong.getEmail() != ong.getEmail()) {
-			ongDB.setEmail(ong.getEmail());
-		}
-		if(ong.getWhatsapp_ong() != ong.getWhatsapp_ong()) {
-			ongDB.setWhatsapp_ong(ong.getWhatsapp_ong());
-		}
-		if(ong.getSobre_ong() != ong.getSobre_ong()) {
-			ongDB.setSobre_ong(ong.getSobre_ong());
-		}
-		if(ong.getArea_atuacao_ong() != ong.getArea_atuacao_ong()) {
-			ongDB.setArea_atuacao_ong(ong.getArea_atuacao_ong());
-		}
-		if(ong.getFacebook_ong() != ong.getFacebook_ong()) {
-			ongDB.setFacebook_ong(ong.getFacebook_ong());
-		}
-		if(ong.getLogradouro_local_ong() != ong.getLogradouro_local_ong()) {
-			ongDB.setLogradouro_local_ong(ong.getLogradouro_local_ong());
-		}
-		if(ong.getNumero_local_ong() != ong.getNumero_local_ong()) {
-			ongDB.setNumero_local_ong(ong.getNumero_local_ong());
-		}
-		if(ong.getComplemento_local_ong() != ong.getComplemento_local_ong()) {
-			ongDB.setComplemento_local_ong(ong.getComplemento_local_ong());
-		}
-		if(ong.getCep_local_ong() != ong.getCep_local_ong()) {
-			ongDB.setCep_local_ong(ong.getCep_local_ong());
-		}
-		if(ong.getEstado() != ong.getEstado()) {
-			ongDB.setEstado(ong.getEstado());
-		}
-		if(ong.getCidade_local_ong() != ong.getCidade_local_ong()) {
-			ongDB.setCidade_local_ong(ong.getCidade_local_ong());
-		}
+		ongBD.setEmail(ong.getEmail());
+		ongBD.setSenha(ong.getSenha());
+		ongBD.setWhatsapp_ong(ong.getWhatsapp_ong());
+		ongBD.setSobre_ong(ong.getSobre_ong());
+		ongBD.setArea_atuacao_ong(ong.getArea_atuacao_ong());
+		ongBD.setFacebook_ong(ong.getFacebook_ong());
+		ongBD.setInstagram_ong(ong.getInstagram_ong());
+		ongBD.setLogradouro_local_ong(ong.getLogradouro_local_ong());
+		ongBD.setNumero_local_ong(ong.getNumero_local_ong());
+		ongBD.setComplemento_local_ong(ong.getComplemento_local_ong());
+		ongBD.setCep_local_ong(ong.getCep_local_ong());
+		ongBD.setEstado(ong.getEstado());
+		ongBD.setCidade_local_ong(ong.getCidade_local_ong());
 		
-		repository.save(ongDB);
-		return ongDB;
+		final UsuarioOng alteraOng = repository.save(ongBD);
+		
+		return alteraOng;
 		
 	}
-	
+
 
 }
