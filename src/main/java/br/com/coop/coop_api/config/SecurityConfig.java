628603e720ad74f086b5af31a838a03a6e8fa3d1
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-11-08 15:50:27 -0300

    Removido Java.util do ServiceConfig

--- src/main/java/br/com/coop/coop_api/config/SecurityConfig.java
+++ src/main/java/br/com/coop/coop_api/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package br.com.coop.coop_api.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
